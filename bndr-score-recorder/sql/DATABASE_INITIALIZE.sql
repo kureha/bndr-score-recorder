bacff1614bfd115d70fc533f5431f41df8e0ed11
Kureha Hisame <kureha@gmail.com>
2019-12-01 11:47:58 +0900

    データベース初期化用のSQL文を追加

---
+++ bndr-score-recorder/sql/DATABASE_INITIALIZE.sql
@@ -0,0 +1,29 @@
+﻿DROP TABLE IF EXISTS "M_MUSIC";
+CREATE TABLE "M_MUSIC" (
+	"id"	TEXT NOT NULL,
+	"title"	TEXT,
+	"difficult"	TEXT,
+	"level"	INTEGER,
+	"insert_date"	TEXT,
+	"update_date"	TEXT,
+	PRIMARY KEY("id")
+)
+;
+
+DROP TABLE IF EXISTS "T_SCORE_RECORD";
+CREATE TABLE "T_SCORE_RECORD" (
+	"id"	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+	"music_id"	TEXT NOT NULL,
+	"perfect"	INTEGER,
+	"great"	INTEGER,
+	"good"	INTEGER,
+	"bad"	INTEGER,
+	"miss"	INTEGER,
+	"total_notes"	INTEGER,
+	"max_combo"	INTEGER,
+	"ex_score"	INTEGER,
+	"image_file_path"	TEXT,
+	"insert_date"	TEXT,
+	"update_date"	TEXT
+)
+;
